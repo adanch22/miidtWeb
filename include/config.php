7136449033b49cd637cb3f5a5d9a95192f66ca2d
Adán Chávez Olivera <achavez@uagro.mx>
2016-08-25 14:41:51 -0500

    mensaje

--- include/config.php
+++ include/config.php
@@ -7,10 +7,10 @@
  */
 
 //SERVER CONFIG
-    define('DB_USERNAME', 'wwwitnov_ciex'); define('DB_PASSWORD', 'Ciex2016'); define('DB_HOST', 'localhost'); define('DB_NAME', 'wwwitnov_ciex');
+   // define('DB_USERNAME', 'wwwitnov_ciex'); define('DB_PASSWORD', 'Ciex2016'); define('DB_HOST', 'localhost'); define('DB_NAME', 'wwwitnov_ciex');
 
 //LOCALHOST CONFIG
-    //define('DB_USERNAME', 'root'); define('DB_PASSWORD', ''); define('DB_HOST', 'localhost'); define('DB_NAME', 'ciex_');
+    define('DB_USERNAME', 'root'); define('DB_PASSWORD', ''); define('DB_HOST', 'localhost'); define('DB_NAME', 'ciex_');
 
 define("GOOGLE_API_KEY", "AIzaSyAv2W47ngdsWsTqDSUeBFaZq3CegFd6jV4");
 
